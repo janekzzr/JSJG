41930237d0e76b14dff237eedf7b102c5da0f702
janekzzr <janekzzr@outlook.com>
2026-09-16 19:12:30 +0200

    Add files via upload

---
+++ grzegorzak_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("mam numer w dzienniku 2"+"<br>");
+document.write("Pierwszy wiersz to: 15 Nie Tak 7 "+"<br>");
+document.write("Drugi wiersz to: 15 Nie Tak 7  "+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("teal").strike().fontsize(7)+"<br>"); 
+document.write(document.lastModified.fontcolor("teal").strike().fontsize(7));
